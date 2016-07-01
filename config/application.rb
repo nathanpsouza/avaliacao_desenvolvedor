31364d646b258a4495739d3ff55b8d006be4d805
Nathan Souza <nathanpsouza@gmail.com>
2016-07-01 18:05:03 -0300

    configure application timezone and locale, rspec generators and add pt-BR i18n file

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,30 @@ module TestNama
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
+    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
+    config.time_zone = 'Brasilia'
+
+    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '*', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = :'pt-BR'
+
+    # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.active_record.raise_in_transactional_callbacks = true
+
+    # Do not generate specs for views and requests. Also, do not generate assets.
+    config.generators do |g|
+      g.helper false
+      g.view_specs false
+      g.assets false
+      g.integration_tool false
+    end
+
+    config.app_generators do |g|
+      g.test_framework :rspec
+    end
+
+    config.autoload_paths << Rails.root.join('lib')
   end
 end

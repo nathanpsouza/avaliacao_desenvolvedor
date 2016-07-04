e5bf02a6b06433042c3b2286fe8227ecfc52c421
Nathan Souza <nathanpsouza@gmail.com>
2016-07-04 11:16:10 -0300

    create parser to read txt files

---
+++ lib/importer/base.rb
@@ -0,0 +1,61 @@
+module Importer
+  class Base
+    attr_accessor :path
+
+    def initialize path      
+      @path = path
+    end
+
+    def get_header
+      begin
+        return File.open(@path, &:readline)
+      rescue Exception => e
+        return ""
+      end
+    end
+
+    def valid?
+      header = get_header
+      return false if header.empty?
+      is_header? header
+    end
+
+    def is_header? string
+      string == "Comprador\tdescrição\tPreço Uniário\tQuantidade\tEndereço\tFornecedor\n"
+    end
+
+    def parse line
+      columns = line.gsub("\n", "").split("\t")
+      raise Exception.new("Invalid line to parse") if columns.size != 6
+
+      {
+        customer_name: columns[0], 
+        description: columns[1], 
+        value: columns[2].to_d, 
+        quantity: columns[3].to_i, 
+        address: columns[4], 
+        provider: columns[5]
+      }
+    end
+
+    def get_rows
+      rows = []
+      File.open(@path, "r") do |infile|
+        while (line = infile.gets)
+          rows << parse(line) unless is_header?(line)
+        end
+      end
+
+      rows
+    end
+
+    def import
+      ActiveRecord::Base.transaction do
+        get_rows.each do |record|
+          order = Order.create(record)
+          raise Exception.new("Invalid record") unless order.persisted?
+        end
+      end
+    end
+  end
+end
---
+++ spec/lib/importer/base_spec.rb
@@ -0,0 +1,74 @@
+require 'rails_helper'
+module Importer
+  RSpec.describe Base do
+    let(:path) { File.join(Rails.root, 'spec', 'support', 'order', 'dados.txt') } 
+    let(:invalid) { File.join(Rails.root, 'spec', 'support', 'order', 'invalid.txt') } 
+    let(:blank_path) { File.join(Rails.root, 'spec', 'support', 'order', 'blank.txt') }
+    let(:no_header_path) { File.join(Rails.root, 'spec', 'support', 'order', 'no_header.txt') }
+    
+    let(:importer) { Base.new(path) }
+
+    describe '.initialize' do
+      it 'instantiate @path with file path' do
+        expect(importer.path).to eq(path)
+      end
+    end
+
+    describe '#valid?' do
+      it 'is valid with header' do
+        expect(importer.valid?).to eq(true)
+      end
+      
+      it 'is invalid without header' do
+        no_header = Base.new(no_header_path)
+        expect(no_header.valid?).to eq(false)
+      end
+
+      it 'is invalid with blank file' do
+        blank = Base.new(blank_path)
+        expect(blank.valid?).to eq(false)
+      end
+    end
+
+    describe '#get_header' do
+      it 'return file header' do
+        expect(importer.get_header).to eq("Comprador\tdescrição\tPreço Uniário\tQuantidade\tEndereço\tFornecedor\n")
+      end
+    end
+
+    describe '#parse' do
+      it 'return order object from string' do
+        expect(importer.parse("João Silva\tR$10 off R$20 of food\t10.0\t2\t987 Fake St\tBob's Pizza")).to eq({
+          customer_name: 'João Silva', description: 'R$10 off R$20 of food', value: 10.0, quantity: 2, address: '987 Fake St', provider: "Bob's Pizza"
+        })
+      end
+
+      it 'raise exception when string is not in valid format' do
+        expect {
+          importer.parse("invalid string")
+        }.to raise_error Exception
+      end
+    end
+
+    describe '#get_rows' do
+      it 'return a array with values from file' do
+        expect(importer.get_rows.size).to eq(4)
+      end
+    end
+
+    describe '#import' do
+      it 'add records from txt to database when all valid' do
+        expect {
+          importer.import
+        }.to change(Order, :count).by(4)
+      end
+
+      it 'raise exception with invalid files' do
+        importer = Base.new(invalid)
+        expect {
+          importer.import
+        }.to raise_error Exception
+      end
+    end
+  end
+end
